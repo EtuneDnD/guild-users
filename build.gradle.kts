301a6af09413ad1d4d04770325b67d0cdc41ae77
AlbertoEE <albertolopezgarciaee@gmail.com>
2022-06-13 22:49:45 +0200

    Fixing UserId Bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar
 }
 
 tasks.getByName<Jar>("jar") {
-    enabled = true
+    enabled = false
 }
 
 allprojects {
@@ -59,3 +59,97 @@ allprojects {
         useJUnitPlatform()
     }
 }
+
+abstract class DeployPostgresSQL : DefaultTask() {
+    @TaskAction
+    fun deployPostgresSQL() {
+        val p = ProcessBuilder(
+            "cmd.exe",
+            "/c",
+            "docker-compose -f docker/docker-compose.yaml up -d db"
+        ).start()
+
+        val results: List<String> = p.errorStream.bufferedReader().readLines()
+
+        results.forEach{
+            println(it)
+        }
+    }
+
+}
+
+abstract class BuildDockerImage : DefaultTask() {
+    @TaskAction
+    fun buildDockerImage() {
+        val p = ProcessBuilder(
+            "cmd.exe",
+            "/c",
+            "docker build -f docker/Dockerfile . -t guild-users:local"
+        ).start()
+
+        val results: List<String> = p.errorStream.bufferedReader().readLines()
+
+        results.forEach{
+            println(it)
+        }
+    }
+}
+
+abstract class DeployDockerCompose : DefaultTask() {
+    @TaskAction
+    fun deployDockerCompose() {
+        val p = ProcessBuilder(
+            "cmd.exe",
+            "/c",
+            "docker-compose -f docker/docker-compose.yaml up -d"
+        ).start()
+
+        val results: List<String> = p.errorStream.bufferedReader().readLines()
+
+        results.forEach{
+            println(it)
+        }
+    }
+
+}
+
+abstract class Asd : DefaultTask() {
+    @TaskAction
+    fun asd() {
+    }
+
+}
+
+tasks.register<DeployPostgresSQL>("deployPostgresSQL"){
+}
+
+tasks.register<BuildDockerImage>("buildDockerImage"){
+    val build = getTasksByName("build", true)
+    dependsOn(build)
+}
+
+tasks.register<DeployDockerCompose>("deployDockerCompose"){
+    val buildDockerImage = tasks["buildDockerImage"]
+
+    dependsOn(buildDockerImage)
+}
+
+tasks.register<Asd>("asd"){
+    val b = tasks["deployPostgresSQL"]
+    val flywayMigrate = getTasksByName("flywayMigrate", true)
+    val a = tasks["deployDockerCompose"]
+
+    dependsOn(flywayMigrate)
+    dependsOn(a)
+    dependsOn(b)
+
+    a.mustRunAfter(flywayMigrate)
+    flywayMigrate.
+}
+
+
+// Levantar POSTGRESS CONTAINER
+// Correr Flyway
+// Build
+// Crear imagen de guild-users
+// Levantar docker compose
---
+++ infrastructure/build.gradle.kts
@@ -0,0 +1,9 @@
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
